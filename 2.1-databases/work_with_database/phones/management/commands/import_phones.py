cb45cded5c3ba5804c5fefa9a59f6597d1d42b2f
NikiLis26 <120452720+NikiLis26@users.noreply.github.com>
2024-02-18 13:01:19 +0300

    3 task

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -6,12 +6,21 @@ from phones.models import Phone
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        pass
+        parser.add_argument('csv_file', type=str, help='phones.csv')
 
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
+        with open('phones.csv', 'r', encoding='utf-8') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            phone = Phone(
+                name=phone['name'],
+                price=phone['price'],
+                image=phone['image'],
+                release_date=phone['release_date'],
+                lte_exists=phone['lte_exists'],
+            )
+            phone.save()
+
+        self.stdout.write(self.style.SUCCESS('Phone data imported successfully'))
+
